f7bb3b0eef4b2656994284164e2ca9853419df00
synertia <synertia@gmail.com>
2015-12-20 11:39:03 +0800

    Update to stop tracking of projectile after leaving field of view.

--- tracker.py
+++ tracker.py
@@ -2,20 +2,19 @@
 
 from graphics import *
 
-
 class Tracker:
-    """This class will create a visual element to track an object on the screen
-    as a visual representation in a window."""
-
-    def __init__(self, window, objToTrack):
-        self.cball = Circle(Point(objToTrack.getX(), objToTrack.getY()), 10)
-        self.cball.setFill("grey")
-        self.cball.setWidth(2)
-        self.cball.draw(window)
-
-    def update(self, window, objToTrack):
-        self.cball.undraw()
-        self.cball = Circle(Point(objToTrack.getX(), objToTrack.getY()), 10)
-        self.cball.setFill("grey")
-        self.cball.setWidth(2)
-        self.cball.draw(window)
+	"""This class will create a visual element to track an object on the screen
+	as a visual representation in a window."""
+	
+	def __init__(self,window,objToTrack):
+		self.cball = Circle(Point(objToTrack.getX(),objToTrack.getY()),10)
+		self.cball.setFill("grey")
+		self.cball.setWidth(2)
+		self.cball.draw(window)
+		
+	def update(self,window,objToTrack):
+		self.cball.undraw()
+		self.cball = Circle(Point(objToTrack.getX(),objToTrack.getY()),10)
+		self.cball.setFill("grey")
+		self.cball.setWidth(2)
+		self.cball.draw(window)
